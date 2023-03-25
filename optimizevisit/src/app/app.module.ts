ea4bf017fad669fe764d2a9b4c042f7de6cb4e06
ada4274 <ada42ada74@gmail.com>
2023-03-25 16:18:22 +0100

    containers

--- optimizevisit/src/app/app.module.ts
+++ optimizevisit/src/app/app.module.ts
@@ -5,14 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatIconModule} from '@angular/material/icon';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { MapComponent } from './components/map/map.component'; 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ////// Angular-material
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
